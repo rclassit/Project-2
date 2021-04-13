8e28dfea4a4931f39c1a063d1b72230d049c7dcb
Lizzy Revels <73864473+LizzyRevels@users.noreply.github.com>
2021-04-13 11:25:47 -0400

    added render_template at top

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import psycopg2 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS, cross_origin
 
 
